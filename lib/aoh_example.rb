94ad81366da55e255c883563ffbb096bb21288d4
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-27 03:25:29 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -19,6 +19,7 @@ def literal_aoh
   # held in the constants
   array = [{:name=>"Don Gately",:occupation=>"Live-in Staff"},{:name=>"Joelle van Dyne",:occupation=>"Radio Personality"},{:name=>"Pat Monteseian",:occupation=>"Staff"},{:name=>"Kate Gompert",:occupation=>"None"},{:name=>"Bruce Green",:occupation=>"Fan of Mildred"}]
 end
+p literal_aoh
 
 def aoh_lookup(aoh, row, key)
 end
