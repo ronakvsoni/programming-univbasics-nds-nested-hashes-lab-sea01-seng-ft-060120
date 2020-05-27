d61535961c59f8663b09b7acac415bda62f33de2
Ronak V Soni <ronakvsoni@gmail.com>
2020-05-27 03:39:34 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -41,6 +41,8 @@ end
 p aoh_lookup(array,nil,nil)
 
 def aoh_update(aoh, row, key, new_value)
+  array[3][:occupation]="Artist"
+  
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
